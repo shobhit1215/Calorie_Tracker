de8eb7cb891807a246029e0d2958491a99ea25df
Shobhit <imshobhit.sb@gmail.com>
2021-06-15 11:58:24 +0530

    Created Consume model

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Food(models.Model):
@@ -10,4 +12,8 @@ class Food(models.Model):
 
     def __str__(self):
         return self.name
+
+class Consume(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    food_consumed = models.ForeignKey(Food,on_delete=models.CASCADE)
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-from .models import Food
+from .models import Food,Consume
 
 
 # Create your views here.
+def index(request):
+
+    if request.method=="POST":
+        food_consumed = request.POST['food_consume']
+        consume = Food.objects.get(name=food_consumed)
+        user = request.user
+        consume = Consume(user=user,food_consumed=consume)
+        consume.save()
+        foods = Food.objects.all()
+
+    
+    else:
+        foods = Food.objects.all()
+    return render(request,'myapp/index.html',{'foods':foods})
